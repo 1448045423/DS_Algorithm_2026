2a12cba05a18a0efb86765fe8a8f292abd3e7c53
1448045423 <1448045423@qq.com>
2026-09-23 12:51:16 +0800

    feat: 完善中序线索二叉树遍历代码

--- Tree/06_Tree.cpp
+++ Tree/06_Tree.cpp
@@ -33,4 +33,49 @@ void visit(ThreadNode *q)
 	}
 	pre=q;
 }
+
+//中序遍历
+//找到中序下的首元素
+ThreadNode *FirstNode(ThreadNode *p) 
+{
+	while(p->ltag==0)
+		p=p->lchild;
+	return p;
+}
+
+//找改节点的后一个
+ThreadNode *NextNode(ThreadNode *p) 
+{
+	if(p->rtag==0)
+		return FirstNode(p->rchild);
+	return p->rchild;
+}
+
+//循环遍历中序线索二叉树
+void InOrder(ThreadNode *T) 
+{
+	for(ThreadNode *p=FirstNode(T);p!=NULL;p=NextNode(p))
+	{
+		visit(p);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  
